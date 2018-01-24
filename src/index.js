284cf10c2d71e4b08cdb65680f67c55604e63553
PJC-1 <jumpzine@gmail.com>
2018-01-23 20:40:46 -0800

    installed react router and defined route for root

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path='/' component={App} />
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
 registerServiceWorker();
